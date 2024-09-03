f00156cb18ee9e40b8536f6d28e397254b8cabba
ted0zh <119053638+ted0zh@users.noreply.github.com>
2024-09-03 13:39:38 +0300

    base

---
+++ backend/expBanking/src/main/java/com/expbanking/expBanking/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.expbanking.expBanking;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ExpBankingApplication.class);
+	}
+
+}

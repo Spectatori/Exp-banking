8f599c7130f135901d781ba2599a831ee1e7d4e0
ted0zh <119053638+ted0zh@users.noreply.github.com>
2024-09-03 13:37:27 +0300

    base

---
+++ expBanking/src/test/java/com/expbanking/expBanking/ExpBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expbanking.expBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

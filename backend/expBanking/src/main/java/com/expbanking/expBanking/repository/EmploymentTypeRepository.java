098e5b2e107f80eed87b55d0e506ed499da70e88
ted0zh <tedi200220@gmail.com>
2024-09-10 14:54:14 +0300

    ...

---
+++ backend/expBanking/src/main/java/com/expbanking/expBanking/repository/EmploymentTypeRepository.java
@@ -0,0 +1,7 @@
+package com.expbanking.expBanking.repository;
+
+import com.expbanking.expBanking.model.EmploymentType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmploymentTypeRepository extends JpaRepository<EmploymentType,Long> {
+}

c5cfe35f81755c3eb525e93352b9c30c9d92ef1a
Dimitrina Nestorova <136362960+anirdm@users.noreply.github.com>
2024-09-14 00:51:51 +0300

    fix: improve responsiveness of account overview page

--- frontend/exp-banking/src/pages/AccountOverviewPage.jsx
+++ frontend/exp-banking/src/pages/AccountOverviewPage.jsx
@@ -122,7 +122,8 @@ const AccountOverviewPage = () => {
     const handleTransactionHistoryClick = (accountId) => {
         setSelectedAccountTransactions(mockTransactionHistory[accountId] || []);
         setSelectedAccount(mockAccounts.find((account) => account.id === accountId));
-        setShowTransactionHistory(true);
+
+        selectedAccount.id === accountId ? setShowTransactionHistory(!showTransactionHistory) : setShowTransactionHistory(true);
     };
 
     return (
@@ -135,33 +136,34 @@ const AccountOverviewPage = () => {
             <div className='mx-6 mb-5'>
                 <h1 className='font-semibold text-2xl text-blue-whale mt-12'>Добре дошли, Име Фамилия</h1>
 
-                <div className='flex flex-col lg:flex-row justify-between items-center'>
-                    <section className='w-fit'>
-                        <div className='flex flex-row justify-between items-center mt-10 relative'>
-                            <h2 className='text-xl font-semibold text-blue-whale'>Наличност по сметки</h2>
-                            <PrimaryButton label='Добави сметка' className='bg-kelly-green' onClick={handleAddAccountButtonClick} />
+                <section className='md:w-fit'>
+                    <div className='flex flex-row justify-between items-center mt-10 relative'>
+                        <h2 className='text-xl font-semibold text-blue-whale'>Наличност по сметки</h2>
+                        <PrimaryButton label='Добави сметка' className='bg-kelly-green' onClick={handleAddAccountButtonClick} />
 
-                            {isAddAccountButtonClicked && (
-                                <AddAccountForm closeForm={closeForm} />
-                            )}
-                        </div>
+                        {isAddAccountButtonClicked && (
+                            <AddAccountForm closeForm={closeForm} />
+                        )}
+                    </div>
 
-                        <div className='flex flex-row'>
-                            <div className='mt-5 space-y-7'>
-                                {mockAccounts.map((account) => (
-                                    <ShadowBox key={account.id} className='h-fit flex-1 justify-between gap-24 items-center'>
-                                        <div className='flex flex-col w-fit'>
-                                            <p className='text-kelly-green font-semibold text-xl self-end'>{account.balance}</p>
-                                            <p className='text-gray-500'>наличност <span className='text-kelly-green'>BGN</span></p>
-                                        </div>
+                    <div className=''>
+                        <div className='mt-5 space-y-7'>
+                            {mockAccounts.map((account) => (
+                                <ShadowBox key={account.id} className='h-fit flex-col  '>
+                                    <div className='flex flex-col gap-5 md:flex-row md:gap-24'>
 
-                                        <div className='flex flex-col w-fit'>
-                                            <p className='font-semibold text-lg self-end'>{account.type}</p>
+                                        <div className='flex flex-col flex-1'>
+                                            <p className='font-semibold text-lg'>{account.type}</p>
                                             <p>{account.IBAN}</p>
                                         </div>
 
+                                        <div className='flex flex-col'>
+                                            <p className='text-kelly-green font-semibold text-xl md:self-end'>{account.balance}</p>
+                                            <p className='text-gray-500'>наличност <span className='text-kelly-green'>BGN</span></p>
+                                        </div>
+
                                         <div className='flex flex-row gap-5'>
-                                            <div className='flex flex-col w-fit'>
+                                            <div className='flex flex-col'>
                                                 <button className='overflow-visible h-16 relative flex flex-col items-center justify-center gap-2 transition-all duration-300 group overflow-hidden'>
                                                     <img src={detailsIcon} className='size-7 transition-transform duration-300 group-hover:translate-y-[-8px]' alt="" />
                                                     <p className='mt-12 absolute text-xs transform translate-y-5 transition-transform duration-300 opacity-0 group-hover:translate-y-0 group-hover:opacity-100'>Детайли</p>
@@ -178,44 +180,41 @@ const AccountOverviewPage = () => {
                                                 </button>
                                             </div>
                                         </div>
-                                    </ShadowBox>
-                                ))}
-                            </div>
-                        </div>
-                    </section>
-
-                    {showTransactionHistory && (
-                        <ShadowBox className='flex flex-col'>
-                            <p className='font-semibold text-lg'>{selectedAccount.type}</p>
-                            <p>{selectedAccount.IBAN}</p>
-
-                            {selectedAccountTransactions.map((transaction) => {
-                                const categoryCircle = categoryColors[transaction.category]?.Circle || null;
-
-                                return (
-                                    <div className='flex gap-16 items-center mt-5'>
-                                        <div className='flex flex-row gap-2'>
-                                            <img src={TransactionIcon} alt="Transaction Icon" />
-                                            <div className='flex flex-col flex-1'>
-                                                <h3>{transaction.merchant}</h3>
-                                                <p>{transaction.date}</p>
-                                            </div>
-                                        </div>
-                                        <p className='flex-1 text-start'>{transaction.description}</p>
-                                        <div className='flex-1 flex items-center justify-end'>
-                                            {categoryCircle && <img src={categoryCircle} className="w-4 h-4" alt="Category Circle" />}
-                                            <p >{transaction.category}</p>
-                                        </div>
-                                        <p className='flex-none'>{transaction.amount} BGN</p>
                                     </div>
-                                )
-                            })}
-                        </ShadowBox>
-                    )}
-                </div>
+
+
+                                    {selectedAccount.id === account.id && showTransactionHistory && (
+                                        <div className='mt-5 flex flex-col w-[35rem] border-t border-t-gray-300 w-full px-5 justify-items-center text-sm md:text-base'>
+                                            {selectedAccountTransactions.map((transaction) => {
+                                                const categoryCircle = categoryColors[transaction.category]?.Circle || null;
+
+                                                return (
+                                                    <div className='flex gap-10 mt-5 items-center'>
+                                                        <div className='flex flex-row gap-2'>
+                                                            <div className='flex flex-col '>
+                                                                <h3>{transaction.merchant}</h3>
+                                                                <p>{transaction.date}</p>
+                                                            </div>
+                                                        </div>
+                                                        <p className='flex-1 text-start'>{transaction.description}</p>
+                                                        <div className='flex-1 flex items-center justify-end'>
+                                                            {categoryCircle && <img src={categoryCircle} className="w-4 h-4" alt="Category Circle" />}
+                                                            <p >{transaction.category}</p>
+                                                        </div>
+                                                        <p className='flex-none'>{transaction.amount} BGN</p>
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                    )}
+                                </ShadowBox>
+                            ))}
+                        </div>
+                    </div>
+                </section>
             </div>
 
-            <section className='flex flex-row bg-space-wolves-grey mt-10 p-10 items-center justify-evenly'>
+            <section className='flex gap-10 flex-row bg-space-wolves-grey mt-10 p-10 items-center justify-evenly'>
                 <button className='max-w-28'>
                     <div className='flex flex-col items-center gap-2 group'>
                         <div className='flex bg-azure border rounded-full size-28 items-center justify-center transition-transform duration-300 ease-in-out transform group-hover:scale-110'>
@@ -225,7 +224,7 @@ const AccountOverviewPage = () => {
                                 className='h-16'
                             />
                         </div>
-                        <h3 className='text-lg font-semibold text-blue-whale leading-5 mt-2'>Създай нов превод </h3>
+                        <h3 className='text-lg font-semibold text-blue-whale leading-5 mt-2'>Нов превод </h3>
                     </div>
                 </button>
 
@@ -238,7 +237,7 @@ const AccountOverviewPage = () => {
                                 className='h-16'
                             />
                         </div>
-                        <h3 className='text-lg font-semibold text-blue-whale leading-5 mt-2'>Кандидатствай за заем</h3>
+                        <h3 className='text-lg font-semibold text-blue-whale leading-5 mt-2'>Потребителски кредит</h3>
                     </div>
                 </button>
 
@@ -251,7 +250,7 @@ const AccountOverviewPage = () => {
                                 className='h-16'
                             />
                         </div>
-                        <h3 className='text-lg font-semibold text-blue-whale leading-5 mt-2'>Кандидатствай за ипотечен кредит</h3>
+                        <h3 className='text-lg font-semibold text-blue-whale leading-5 mt-2'>Ипотечен кредит</h3>
                     </div>
                 </button>
             </section>

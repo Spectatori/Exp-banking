2909e1af27393073ee0594423dd2706f6b1c5637
Dimitrina Nestorova <136362960+anirdm@users.noreply.github.com>
2024-09-13 21:32:57 +0300

    style: add burger menu for smaller screens

--- frontend/exp-banking/src/components/nav-bar/authenticated/AuthNavbar.jsx
+++ frontend/exp-banking/src/components/nav-bar/authenticated/AuthNavbar.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import CustomNavLink from '../CustomNavLink'
+import React, { useState } from 'react';
+import CustomNavLink from '../CustomNavLink';
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const AuthNavbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <nav>
-            <ul className="flex gap-8 text-dark-blue font-semibold text-sm [&>*]:px-3 mt-5 ml-3">
+        <nav className='relative'>
+            <button 
+                className='md:hidden absolute top-0 right-4 mt-3 mr-3'
+                onClick={toggleMenu}
+            >
+                <GiHamburgerMenu size={24} />
+            </button>
+
+            <ul
+                className={`absolute top-12 right-5 w-96 rounded-md space-y-3 p-5 font-semibold bg-white border border-gray-300 shadow-md md:w-full md:shadow-none md:static md:space-y-0 md:flex md:items-center md:gap-8 md:text-dark-blue md:text-sm md:mt-5 md:ml-3 md:bg-transparent md:border-none ${isMenuOpen ? 'block' : 'hidden'}`}
+            >
                 <CustomNavLink url='/' title='НАЧАЛО' />
                 <CustomNavLink url='/' title='СМЕТКИ И ДЕПОЗИТИ' />
                 <CustomNavLink url='/' title='КАРТИ' />
@@ -12,7 +28,9 @@ const AuthNavbar = () => {
                 <CustomNavLink url='/' title='КРЕДИТИ' />
             </ul>
         </nav>
-    )
-}
+    );
+};
 
-export default AuthNavbar
+export default AuthNavbar;
+
+

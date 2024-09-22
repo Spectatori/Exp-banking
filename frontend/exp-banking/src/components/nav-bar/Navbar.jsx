fea492e30989cc14b622389f83c20b2f7804e4f3
Spectatori <cavdar2002@gmail.com>
2024-09-22 17:05:40 +0300

    fix: Fixed responsiveness in the unauthorized navbar

--- frontend/exp-banking/src/components/nav-bar/Navbar.jsx
+++ frontend/exp-banking/src/components/nav-bar/Navbar.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CustomNavLink from "./CustomNavLink";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
 const Navbar = () => {
+        const [isMenuOpen, setIsMenuOpen] = useState(false);
+    
+        const toggleMenu = () => {
+            setIsMenuOpen(!isMenuOpen);
+        };
+
     return (
-        <nav className="flex gap-12 bg-ghost-white p-1 items-center">
-            <Link
-                to='/'
-            >
-                <img src="/logo.png" className="w-16 h-14 ml-2" alt="" />
-            </Link>
-            
-            <ul className="flex gap-8 text-dark-blue font-semibold [&>*]:px-3">
-                <CustomNavLink url='/za-teb' title='За теб'/>
-                <CustomNavLink url='/za-tvoya-biznes' title='За твоя бизнес'/>
-                <CustomNavLink url='/za-exp-bank' title='За Exp Bank'/>
-            </ul>
+        <nav className="pl-6 flex bg-ghost-white pt-4 max-xl:pl-0 max-xl:gap-6 max-xl:flex-col">
+            <div className="flex max-xl:w-full max-xl:justify-center">
+                <img src="/logo.png" className="w-20" alt="" />
+            </div>
+            <nav className='flex w-full flex-col gap-5 items-center'>
+                <button 
+                    className='md:hidden'
+                    onClick={toggleMenu}
+                >
+                    <GiHamburgerMenu  size={24} />
+                </button>
+
+                <ul
+                    className={`max-xl:justify-center z-50 rounded-md w-72 text-blue-whale space-y-3 p-5 font-semibold
+                        bg-white border border-gray-300 shadow-md md:w-full md:shadow-none md:static md:space-y-0 md:flex 
+                        md:items-center md:gap-8 md:text-dark-blue md:text-sm  md:bg-transparent md:border-none 
+                        ${isMenuOpen ? 'block' : 'hidden'}`}
+                >
+                    <CustomNavLink url='/za-teb' title='За теб'/>
+                    <CustomNavLink url='/za-tvoya-biznes' title='За твоя бизнес'/>
+                    <CustomNavLink url='/za-exp-bank' title='За Exp Bank'/>
+                </ul>
+            </nav>
         </nav>
     );
 };

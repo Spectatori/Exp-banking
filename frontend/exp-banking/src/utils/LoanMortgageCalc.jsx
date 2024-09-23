0bd5dae845b66530cdc453fa2a640709fcaccb6e
Spectatori <cavdar2002@gmail.com>
2024-09-23 22:55:21 +0300

    feat: calculator now works with months instead of hard coded years

--- frontend/exp-banking/src/utils/LoanMortgageCalc.jsx
+++ frontend/exp-banking/src/utils/LoanMortgageCalc.jsx
@@ -1,20 +1,12 @@
-const calculateLoan = (principal, years, interestRate) => {
-  let monthlyPayment = principal;
-  let overallPayment = principal;
-  const resultsArray = []; 
+const calculateLoan = (principal, period, interestRate) => {
+  const overallPayment = principal * (1 + interestRate / 100* period); // Total payment after all months
+  const monthlyPayment = overallPayment / period; // Monthly payment
 
-  for (let year = 1; year <= years; year++) {
-      overallPayment *= (1 + interestRate / 100);
-      monthlyPayment = overallPayment / (12 * year);
-      resultsArray.push({ 
-          year, 
-          overallPayment: overallPayment.toFixed(2), 
-          monthlyPayment: monthlyPayment.toFixed(2) 
-      });
-      console.log(`Year ${year}: ${overallPayment.toFixed(2)} BGN`);
-  }
-
-  return resultsArray;
+  return [{
+      overallPayment: overallPayment.toFixed(2),
+      period,
+      monthlyPayment: monthlyPayment.toFixed(2),
+  }];
 };
 
 export default calculateLoan;

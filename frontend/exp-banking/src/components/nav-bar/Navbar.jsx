997c87230af0482f5124ab9484dc63a6d792e00e
Spectatori <cavdar2002@gmail.com>
2024-09-25 15:38:18 +0300

    fix: fixed logged in navbar to show in only 1 row

--- frontend/exp-banking/src/components/nav-bar/Navbar.jsx
+++ frontend/exp-banking/src/components/nav-bar/Navbar.jsx
@@ -20,7 +20,6 @@ const Navbar = () => {
             ? (
                 <nav>
                     <InnerHeader />
-                    <AuthNavbar />
                 </nav>
             )
             : (
--- frontend/exp-banking/src/components/nav-bar/authenticated/InnerHeader.jsx
+++ frontend/exp-banking/src/components/nav-bar/authenticated/InnerHeader.jsx
@@ -4,6 +4,7 @@ import { FaUserAlt } from "react-icons/fa";
 import { MdLogout } from "react-icons/md";
 import { useUserStore } from '../../../stores/AuthStore';
 import { useNavigate } from 'react-router-dom';
+import AuthNavbar from './AuthNavbar';
 
 const InnerHeader = () => {
     const { user, clearUser } = useUserStore();
@@ -16,12 +17,15 @@ const InnerHeader = () => {
 
     return (
         <nav className="flex justify-between bg-ghost-white p-1 items-center pt-3">
+            <div className='flex'>
             <Link
                 to='/account-overview'
             >
                 <img src="/logo.png" className="w-16 h-14 ml-2" alt="" />
             </Link>
-
+            <AuthNavbar />
+            </div>
+            
             <div>
                 <div className='flex flex-row gap-5 items-center'>
                     <FaUserAlt size={22} className='text-blue-whale' />
